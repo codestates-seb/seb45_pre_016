2d204afb69c29b71b2b966172bcd4791b23faa1c
kuetiipp <tjstjsgoqgoq@naver.com>
2023-08-11 11:19:32 +0900

    init:front

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
