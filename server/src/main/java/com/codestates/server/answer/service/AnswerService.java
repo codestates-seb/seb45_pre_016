21b1e9637a5a4c78f51aecf955d9acdbe60cfb66
in9in9 <in551@naver.com>
2023-08-11 19:13:17 +0900

    feat : answer CRUD 기능 추가

---
+++ server/src/main/java/com/codestates/server/answer/service/AnswerService.java
@@ -0,0 +1,21 @@
+package com.codestates.server.answer.service;
+
+import com.codestates.server.answer.entity.Answer;
+import com.codestates.server.answer.repository.AnswerRepository;
+import com.codestates.server.user.entity.User;
+import com.codestates.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+// 예외처리 추가 예정.
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AnswerService {
+
+}
