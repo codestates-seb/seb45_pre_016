6907bf058372cf3d9d779eff83fc112ea26ccd99
luminousol <luminousolding@gmail.com>
2023-08-15 08:48:08 +0900

    feat: Spring security - SecurityConfiguration 클래스 Custom Configuration 추가

--- server/src/main/java/com/codestates/server/security/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/server/security/config/SecurityConfiguration.java
@@ -1,8 +1,14 @@
 package com.codestates.server.security.config;
 
+import com.codestates.server.security.auth.filter.JwtAuthenticationFilter;
+import com.codestates.server.security.auth.jwt.JwtTokenizer;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +21,11 @@ import java.util.Arrays;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtTokenizer jwtTokenizer;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -25,6 +35,8 @@ public class SecurityConfiguration {
                 .cors(withDefaults())   // SecurityConfiguration Bean 이용
                 .formLogin().disable()
                 .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())    // CustomFilterConfigurer 인스턴스 생성
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 );
@@ -45,5 +57,23 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 구성한 CORS 정책 적용
         return source;
+    }
+
+    /*
+      * JwtAuthenticationFilter 등록하는 CustomFilterConfigurer 클래스
+      *
+     */
+    private class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/users/login");   // ⏹️  request URL 체크
+
+            builder.addFilter(jwtAuthenticationFilter); // Spring Security Filter Chain에 추가
+
+        }
     }
 }
