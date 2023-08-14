f3f52fbbdeb3c5c8097fe60b18d2b2e4f003c3e5
luminousol <luminousolding@gmail.com>
2023-08-15 05:24:20 +0900

    feat: security jwt - JwtTokenizer 추가

---
+++ server/src/main/java/com/codestates/server/security/auth/jwt/JwtTokenizer.java
@@ -0,0 +1,11 @@
+package com.codestates.server.security.auth.jwt;
+
+import io.jsonwebtoken.io.Encoders;
+
+public class JwtTokenizer {
+    public String encodeBase64SecretKey(String secretKey) {
+        return null;
+//        return Encoders.BASE64.
+
+    }
+}
