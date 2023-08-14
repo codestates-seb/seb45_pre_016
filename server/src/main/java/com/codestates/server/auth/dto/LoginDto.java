b5bae7bd71d10166e7de2bb2900d3b73c88d3da9
luminousol <luminousolding@gmail.com>
2023-08-15 06:01:06 +0900

    feat: security jwt - SecurityConfiguration 추가

--- server/src/main/java/com/codestates/server/auth/dto/LoginDto.java
+++
@@ -1,4 +0,0 @@
-package com.codestates.server.auth.dto;
-
-public class LoginDto {
-}
---
+++ server/src/main/java/com/codestates/server/security/auth/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.codestates.server.security.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+
+    private String userName;
+
+    private String password;
+
+}
