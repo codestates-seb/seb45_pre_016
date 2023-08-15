ad8d9f4e21218c977a80fd38164bf96f2fd16c1a
luminousol <luminousolding@gmail.com>
2023-08-15 09:20:19 +0900

    comment: 주석 처리 - ErrorResponse(예외처리) 아직 안 함

--- server/src/main/java/com/codestates/server/security/auth/handler/MemberAuthenticationFailureHandler.java
+++ server/src/main/java/com/codestates/server/security/auth/handler/MemberAuthenticationFailureHandler.java
@@ -1,41 +1,41 @@
-package com.codestates.server.security.auth.handler;
-
-import com.google.gson.Gson;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Slf4j
-public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
-
-    /*
-      * 인증 실패 시, 로그 기록 또는 error response 전송
-     */
-    @Override
-    public void onAuthenticationFailure(HttpServletRequest request,
-                                        HttpServletResponse response,
-                                        AuthenticationException exception) {
-
-        log.error("#❌# Authentication failed: {}", exception.getMessage());
-
-        sendErrorResponse(response);
-    }
-
-    private void sendErrorResponse(HttpServletResponse response) throws IOException {
-
-        Gson gson = new Gson();
-
-        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED);
-
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
-    }
-}
+//package com.codestates.server.security.auth.handler;
+//
+//import com.google.gson.Gson;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Slf4j
+//public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
+//
+//    /*
+//      * 인증 실패 시, 로그 기록 또는 error response 전송
+//     */
+//    @Override
+//    public void onAuthenticationFailure(HttpServletRequest request,
+//                                        HttpServletResponse response,
+//                                        AuthenticationException exception) {
+//
+//        log.error("#❌# Authentication failed: {}", exception.getMessage());
+//
+//        sendErrorResponse(response);
+//    }
+//
+//    private void sendErrorResponse(HttpServletResponse response) throws IOException {
+//
+//        Gson gson = new Gson();
+//
+//        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED);
+//
+//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//        response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
+//    }
+//}
